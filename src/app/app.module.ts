b254d4a5397b39906dcda9dcae0b7c0dae0fcc2c
jin <jalicorich@gmail.com>
2022-11-22 18:11:14 +0900

    update: start of hero tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
