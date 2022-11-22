4cfa1451b6da3400cb3ea82ce2b6e4525485ebbb
jin <jalicorich@gmail.com>
2022-11-22 18:11:23 +0900

    update: start of hero tutorial

---
+++ src/app/shared/message/message.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageComponent } from './message.component';
+
+
+
+@NgModule({
+  declarations: [
+    MessageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MessageModule { }
